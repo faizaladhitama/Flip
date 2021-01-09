11d244fad880bfcea539f6c7b314e81ebb328af9
Faizal Adhitama Prabowo <id_faaprabowo@id.carrefour.com>
2021-01-09 23:19:55 +0700

    Create migration

---
+++ database/migrations/2021_01_09_160953_create_disbursements_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDisbursementsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('disbursements', function (Blueprint $table) {
+            $table->id();
+            $table->string('disbursement_id');
+            $table->bigInteger('amount');
+            $table->string('status');
+            $table->dateTime('disbursement_timestamp');
+            $table->string('bank_code');
+            $table->string('account_number');
+            $table->string('beneficiary_name');
+            $table->remark('sample_remark');
+            $table->text('receipt');
+            $table->dateTime('disbursement_time_served');
+            $table->integer('fee');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('disbursements');
+    }
+}
