5320ad6532c01f860d836ab85bb5d9976c5ae754
Faizal Adhitama Prabowo <id_faaprabowo@id.carrefour.com>
2021-01-10 01:02:46 +0700

    Add class for send request to flip

---
+++ app/Integration/SlightlyBig.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Integration;
+use App\Helper\CUrl;
+
+class SlightlyBig
+{
+	protected $secretKey = 'HyzioY7LP6ZoO7nTYKbG8O4ISkyWnX1JvAEVAhtWKZumooCzqp41';
+	protected $baseUrl = 'https://nextar.flip.id';
+	protected $authorization;
+
+	public function __construct()
+	{
+		$this->authorization = base64_encode($secretKey . ":");
+	}
+
+	private function getHeader(){
+		return [
+			'Authorization' => ['Basic '. $this->authorization],
+			'Content-Type' => 'application/x-www-form-urlencoded'
+		]
+	}
+
+	public function sendDisbursement($data){
+		$url = "/disburse";
+		$curl = new CUrl();
+		$curl->setMethod('POST');
+		$curl->setUrl($url);
+		$curl->setHeader($this->getHeader());
+		$curl->setBody($data);
+
+		$response = $curl->sendRequest();
+	}
+
+	public function getDisbursement($transaction_id){
+		$url = "/disburse/$transaction_id";
+		$curl = new CUrl();
+		$curl->setMethod('GET');
+		$curl->setUrl($url);
+		$curl->setHeader($this->getHeader());
+		$curl->setRequestParamter([]);
+
+		$response = $curl->sendRequest();
+	}
+}
