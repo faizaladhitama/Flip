4d7f6be920eb9910798eaea9e994110d1f0f5cb6
Faizal Adhitama Prabowo <id_faaprabowo@id.carrefour.com>
2021-01-10 22:39:47 +0700

    Scheduler for get data asynchronously

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -4,6 +4,9 @@ namespace App\Console;
 
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use App\Models\Disbursement as DisbursementModel;
+use App\Disbursement;
+use App\Integration\SlightlyBig;
 
 class Kernel extends ConsoleKernel
 {
@@ -24,7 +27,34 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')->hourly();
+        $schedule->call(function () {
+            $disbursements = DisbursementModel::where('status', 'pending')->get();
+
+            foreach ($disbursements as $disbursementModel) {
+                $slightlyBig = new SlightlyBig();
+                $disbursement = $slightlyBig->getDisbursement($disbursementModel->disbursement_id);
+
+                print_r($disbursement);
+                
+                $disbursementModel->disbursement_id = $disbursement->id;
+                $disbursementModel->amount = $disbursement->amount;
+                $disbursementModel->status = $disbursement->status;
+                $disbursementModel->disbursement_timestamp = $disbursement->timestamp;
+                $disbursementModel->bank_code = $disbursement->bank_code;
+                $disbursementModel->account_number = $disbursement->account_number;
+                $disbursementModel->beneficiary_name = $disbursement->beneficiary_name;
+                $disbursementModel->remark = $disbursement->remark;
+                $disbursementModel->receipt = $disbursement->receipt;
+
+                if($disbursement->time_served != '0000-00-00 00:00:00'){
+                    $disbursementModel->time_served = $disbursement->time_served;
+                }
+
+                $disbursementModel->fee = $disbursement->fee;
+
+                $disbursementModel->save();
+            }
+        })->everyMinute();
     }
 
     /**
